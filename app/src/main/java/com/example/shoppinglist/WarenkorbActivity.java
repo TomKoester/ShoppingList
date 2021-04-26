866462133d85177fc45fa87ed5c324dea02ce4a6
KoesterJannik <74828011+KoesterJannik@users.noreply.github.com>
2021-04-26 10:22:38 +0200

    Recycler ausgebaut

--- app/src/main/java/com/example/shoppinglist/WarenkorbActivity.java
+++ app/src/main/java/com/example/shoppinglist/WarenkorbActivity.java
@@ -12,27 +12,11 @@ import android.widget.TextView;
 import java.util.ArrayList;
 
 public class WarenkorbActivity extends AppCompatActivity {
-    private RecyclerView mRecyclerView;
-    private RecyclerView.Adapter mAdapter;
-    private  RecyclerView.LayoutManager mLayoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_warenkorb);
-        ArrayList<ExampleItem> exampleItems = new ArrayList<>();
-        exampleItems.add(new ExampleItem(R.drawable.ic_baseline_fastfood_24,"Line 1", "Line 2"));
-        exampleItems.add(new ExampleItem(R.drawable.ic_baseline_architecture_24,"Line 3", "Line 4"));
-        exampleItems.add(new ExampleItem(R.drawable.ic_android,"Line 5", "Line 6"));
-        //Intent intent = getIntent();
-        //Bundle args = intent.getBundleExtra("BUNDLE");
-        mRecyclerView.findViewById(R.id.recyclerView);
-        mRecyclerView.setHasFixedSize(true);
-        mLayoutManager = new LinearLayoutManager(this);
-        mAdapter = new ExampleAdapter(exampleItems);
-
-        mRecyclerView.setLayoutManager(mLayoutManager);
-        mRecyclerView.setAdapter(mAdapter);
 
     }
     public void listAllitems(){
